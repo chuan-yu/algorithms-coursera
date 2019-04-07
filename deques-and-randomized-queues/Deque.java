6084f80d6d214ed67823882969359c7d7997b1ab
Chuan Yu <yuchuan0327@gmail.com>
2019-04-07 14:54:32 +0800

    initial commit

---
+++ deques-and-randomized-queues/Deque.java
@@ -0,0 +1,129 @@
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Deque<Item> implements Iterable<Item> {
+    private Node first;
+    private Node last;
+    private int N;
+
+    class Node {
+        Item item;
+        Node next;
+        Node previous;
+    }
+
+    class DequeIterator implements Iterator<Item> {
+        private Node current = first;
+        public boolean hasNext() { return current != null; }
+        public Item next() {
+            Item item = current.item;
+            current = current.next;
+            return item;
+        }
+        public void remove() {
+            throw new UnsupportedOperationException("remve() is not supported");
+        }
+    }
+
+    public Deque() {
+        first = null;
+        last = null;
+        N = 0;
+    }
+
+    public boolean isEmpty() {
+        return first == null;
+    }
+
+    public int size() {
+        return N;
+    }
+
+    public void addFirst(Item item) {
+        validateItem(item);
+        Node newNode = new Node();
+        newNode.item = item;
+
+        if (isEmpty()) {
+            first = newNode;
+            last = newNode;
+        } else {
+            newNode.next = first;
+            first.previous = newNode;
+            first = newNode;
+        }
+        N++;
+    }
+
+    public void addLast(Item item) {
+        validateItem(item);
+        Node newNode = new Node();
+        newNode.item = item;
+
+        if (isEmpty()) {
+            first = newNode;
+            last = newNode;
+        } else {
+            last.next = newNode;
+            newNode.previous = last;
+            last = newNode;
+        }
+        N++;
+    }
+
+    public Item removeFirst() {
+        if (isEmpty()) {
+            throw new NoSuchElementException("Deque is empty");
+        }
+
+        Item item = first.item;
+
+        if (N == 1) {
+            first = null;
+            last = null;
+        } else {
+            first = first.next;
+            first.previous = null;
+        }
+        N--;
+        return item;
+    }
+
+    public Item removeLast() {
+        if (isEmpty()) {
+            throw new NoSuchElementException(
+                    "The deque is empty."
+            );
+        }
+        Item item = last.item;
+
+        if (N == 1) {
+            first = null;
+            last = null;
+        } else {
+            last = last.previous;
+            last.next = null;
+        }
+        N--;
+        return item;
+    }
+
+    public Iterator<Item> iterator() {
+        return new DequeIterator();
+    }
+
+    private Item getFirstItem() {
+        return first == null ? null : first.item;
+    }
+
+    private Item getLastItem() {
+        return last == null ? null : last.item;
+    }
+
+    private void validateItem(Item item) {
+        if (item == null) {
+            throw new IllegalArgumentException(
+                    "Item to be inserted cannot be null");
+        }
+    }
+}
---
+++ deques-and-randomized-queues/DequeTests.java
@@ -0,0 +1,205 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Method;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DequeTests {
+    private Deque<String> deque;
+
+    @BeforeEach
+    public void createDeque() {
+        deque = new Deque<>();
+    }
+
+    @Test
+    void createDequeShouldSucceed() {
+        assertNotNull(deque);
+    }
+
+    @Test
+    void newlyCreatedDequeShouldBeEmpty() {
+        assertTrue(deque.isEmpty());
+        assertEquals(deque.size(), 0);
+    }
+
+    @Test
+    void addFirstNullShouldThrow() {
+        assertThrows(IllegalArgumentException.class,
+                () -> deque.addFirst(null));
+    }
+
+    @Test
+    void addFirstToEmptyDequeShouldSucceed() {
+        String item = "test string";
+        deque.addFirst(item);
+        assertEquals(deque.size(), 1);
+        String firstItem = getFirst(deque);
+        String lastItem = getLast(deque);
+        assertEquals(firstItem, item);
+        assertEquals(lastItem, item);
+    }
+
+    @Test
+    void addFirstMultipleItemsShouldSucceed() {
+        final int N = 10;
+        for (int i = 0; i < N; i++) {
+            deque.addFirst(Integer.toString(i));
+        }
+        assertEquals(deque.size(), N);
+        String firstItem = getFirst(deque);
+        assertEquals(firstItem, Integer.toString(N-1));
+        String lastItem = getLast(deque);
+        assertEquals(lastItem, "0");
+    }
+
+    @Test
+    void addLastNullShouldThrowException() {
+        assertThrows(IllegalArgumentException.class,
+                () -> deque.addLast(null));
+    }
+
+    @Test
+    void addLastToEmptyDequeShouldSucceed() {
+        String item = "test string";
+        deque.addLast(item);
+        assertEquals(deque.size(), 1);
+        String firstItem = getFirst(deque);
+        String lastItem = getLast(deque);
+        assertEquals(firstItem, item);
+        assertEquals(lastItem, item);
+    }
+
+    @Test
+    void addLastMultipleItemsShouldSucceed() {
+        final int N = 10;
+        for (int i = 0; i < N; i++) {
+            deque.addLast(Integer.toString(i));
+        }
+        assertEquals(deque.size(), N);
+        assertEquals(getFirst(deque), "0");
+        assertEquals(getLast(deque), Integer.toString(N - 1));
+    }
+
+    @Test
+    void removeFirstFromEmptyDequeShouldThrow() {
+        assertThrows(NoSuchElementException.class,
+                () -> deque.removeFirst());
+    }
+
+    @Test
+    void removeFirstFromDequeShouldSucceed() {
+        final int N = 5;
+        for (int i = 0; i < N; i++) {
+            deque.addFirst(Integer.toString(i));
+        }
+
+        for (int i = N-1; i >= 0; i--) {
+            String item = deque.removeFirst();
+            assertEquals(item, Integer.toString(i));
+            assertEquals(deque.size(), i);
+        }
+
+        assertNull(getFirst(deque));
+        assertNull(getLast(deque));
+    }
+
+    @Test
+    void removeLastFromEmptyDequeShouldThrow() {
+        assertThrows(NoSuchElementException.class,
+                () -> deque.removeFirst());
+    }
+
+    @Test
+    void removeLastFromDequeShouldSucceed() {
+        final int N = 5;
+        for (int i = 0; i < N; i++) {
+            deque.addLast(Integer.toString(i));
+        }
+
+        for (int i = N-1; i >= 0; i--) {
+            String item = deque.removeLast();
+            assertEquals(item, Integer.toString(i));
+            assertEquals(deque.size(), i);
+        }
+
+        assertNull(getFirst(deque));
+        assertNull(getLast(deque));
+    }
+
+    @Test
+    void addFirstThenRemoveLastShouldSucceed() {
+        final int N = 5;
+        for (int i = 0; i < N; i++) {
+            deque.addFirst(Integer.toString(i));
+        }
+
+        for (int i = 0; i < N; i++) {
+            String item = deque.removeLast();
+            assertEquals(item, Integer.toString(i));
+            assertEquals(deque.size(), N - i - 1 );
+        }
+
+        assertNull(getFirst(deque));
+        assertNull(getLast(deque));
+    }
+
+    @Test
+    void addLastThenRemoveFirstShouldSucceed() {
+        final int N = 5;
+        for (int i = 0; i < N; i++) {
+            deque.addLast(Integer.toString(i));
+        }
+
+        for (int i = 0; i < N; i++) {
+            String item = deque.removeFirst();
+            assertEquals(item, Integer.toString(i));
+            assertEquals(deque.size(), N - i - 1 );
+        }
+
+        assertNull(getFirst(deque));
+        assertNull(getLast(deque));
+    }
+
+    @Test
+    void iteratorOverDequeShouldSucceed() {
+        for (int i = 0; i < 5; i++) {
+            deque.addLast(Integer.toString(i));
+        }
+        int n = 0;
+        for (String s: deque) {
+            assertEquals(s, Integer.toString(n));
+            n++;
+        }
+    }
+
+
+    private String getFirst(Deque deque) {
+        String firstItem = null;
+        try {
+            Method getFirstItem = deque.getClass().getDeclaredMethod("getFirstItem");
+            getFirstItem.setAccessible(true);
+            firstItem = (String) getFirstItem.invoke(deque);
+        } catch (Exception e) {
+            System.err.println(e.toString());
+            fail("Should not throw");
+        }
+        return firstItem;
+    }
+
+    private String getLast(Deque deque) {
+        String lastItem = null;
+        try {
+            Method getLastItem = deque.getClass().getDeclaredMethod("getLastItem");
+            getLastItem.setAccessible(true);
+            lastItem = (String) getLastItem.invoke(deque);
+        } catch (Exception e) {
+            System.err.println(e.toString());
+            fail("Should not throw");
+        }
+        return lastItem;
+    }
+}
